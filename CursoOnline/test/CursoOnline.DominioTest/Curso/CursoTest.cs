1c93ebbdcc2b2310c31e3741ff0824c663de804c
Santi L <lucassantieng@gmail.com>
2019-06-14 17:35:27 -0300

    Entendendo o xUnit

---
+++ CursoOnline/test/CursoOnline.DominioTest/Curso/CursoTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Curso
+{
+    public class CursoTest
+    {
+
+        [Fact]
+        public void CriarCurso()
+        {
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+            Assert.Equal(nome, curso.Nome);
+            Assert.Equal(cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(publicoAlgo, curso.PublicoAlvo);
+            Assert.Equal(valor, curso.Valor);
+        }
+    }
+
+    public class Curso {
+
+        public Curso() {
+            throw new NotImplementedException();
+        }
+
+    }
+}
